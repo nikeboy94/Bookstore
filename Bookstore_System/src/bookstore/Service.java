5960b997fb79c129413faae4f34055bc4202e494
nikeboy94 <nikeshdhokia@gmail.com>
2017-02-06 16:50:36 +0000

    added constructor code

--- Bookstore_System/src/bookstore/Service.java
+++ Bookstore_System/src/bookstore/Service.java
@@ -6,11 +6,15 @@ public class Service {
 	
 	HashMap<String,Book> bookMap = new HashMap<String,Book>();
 	
-	Book spider = new Book(1, "Spider-Man", "Stan Lee", "Comic");
-	Book green = new Book(2, "Green Eggs and Ham", "Dr Seuss", "Childrens");
-	Book harry = new Book(3, "Harry Potter and the Chamber of Secrets", "J.K. Rowling", "Fantasy");
+	Book book1 = new Book(1, "Spider-Man", "Stan Lee", "Comic");
+	Book book2 = new Book(2, "Green Eggs and Ham", "Dr Seuss", "Childrens");
+	Book book3 = new Book(3, "Harry Potter and the Chamber of Secrets", "J.K. Rowling", "Fantasy");
 		
-	public Service() {}
+	public Service() {
+		bookMap.put("spider", book1);
+		bookMap.put("green", book2);
+		bookMap.put("harry", book3);
+	}
 	
 	
 
